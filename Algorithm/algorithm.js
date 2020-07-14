dea3bad4cbf2e314eed34ab52935d92ab5fdec0c
Ljl <L@AAAAAMacBook-Pro.local>
2020-07-14 11:24:54 +0800

    2020.7.14算法记录

--- Algorithm/algorithm.js
+++ Algorithm/algorithm.js
@@ -296,41 +296,105 @@ var result = countSmaller([5,2,6,1])
 console.log(result);
 
 
-
-const countSmaller = (nums) => {
-    const counts = new Array(nums.length).fill(0);
-    let indexedNums = new Array(nums.length); // indexedNums[i]包含元素的位置信息
-    for (let i = 0; i < indexedNums.length; i++) { // 让大家都能看懂 就没用JS的map
-      indexedNums[i] = { 
-        value: nums[i],
-        index: i
-      };
-    }
-    const mergeSort = (left, right) => {
-      if (right - left <= 1) return indexedNums.slice(left, right);// 不用sort了
-      const pivot = (left + right) >>> 1;
-    //   左边
-      const leftPart = mergeSort(left, pivot);
-    //   右边
-      const rightPart = mergeSort(pivot, right);
-      const merged = [];
-      let i = 0;
-      let j = 0;
-      while (i < leftPart.length) { // 遍历左边部分的元素
-        while (j < rightPart.length &&  rightPart[j].value <leftPart[i].value ) {
-          // 考察右边部分的元素，遇到小于当前左边元素的，推入merged数组
-          merged.push(rightPart[j]); 
-          j++; // 统计当前右边元素中，比当前左边元素小的元素个数
-        }
-        counts[leftPart[i].index] += j; // 在递归中累加j，统计出右边元素比它小的个数
-        merged.push(leftPart[i]); // 较小的进来后，自己也进去了
-        i++;                      // 考察下一个左边元素
-      }
-      // rightPart[j]比左边元素都大，while结束，将它和它后面的元素推入merged数组，继续递归
-      merged.push(...rightPart.slice(j));
-      return merged;
-    };
-    mergeSort(0, indexedNums.length);
-    return counts;
-  };
+// const countSmaller = (nums) => {
+//     const counts = new Array(nums.length).fill(0);
+//     let indexedNums = new Array(nums.length); // indexedNums[i]包含元素的位置信息
+//     for (let i = 0; i < indexedNums.length; i++) { // 让大家都能看懂 就没用JS的map
+//       indexedNums[i] = { 
+//         value: nums[i],
+//         index: i
+//       };
+//     }
+//     const mergeSort = (left, right) => {
+//       if (right - left <= 1) return indexedNums.slice(left, right);// 不用sort了
+//       const pivot = (left + right) >>> 1;
+//     //   左边
+//       const leftPart = mergeSort(left, pivot);
+//     //   右边
+//       const rightPart = mergeSort(pivot, right);
+//       const merged = [];
+//       let i = 0;
+//       let j = 0;
+//       while (i < leftPart.length) { // 遍历左边部分的元素
+//         while (j < rightPart.length &&  rightPart[j].value <leftPart[i].value ) {
+//           // 考察右边部分的元素，遇到小于当前左边元素的，推入merged数组
+//           merged.push(rightPart[j]); 
+//           j++; // 统计当前右边元素中，比当前左边元素小的元素个数
+//         }
+//         counts[leftPart[i].index] += j; // 在递归中累加j，统计出右边元素比它小的个数
+//         merged.push(leftPart[i]); // 较小的进来后，自己也进去了
+//         i++;                      // 考察下一个左边元素
+//       }
+//       // rightPart[j]比左边元素都大，while结束，将它和它后面的元素推入merged数组，继续递归
+//       merged.push(...rightPart.slice(j));
+//       return merged;
+//     };
+//     mergeSort(0, indexedNums.length);
+//     return counts;
+//   };
   
+
+//MARK:2020.07.13
+  /**
+   * 给定两个数组，编写一个函数来计算它们的交集。
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number[]}
+ */
+var intersect = function(nums1, nums2) {
+
+    nums1 = nums1.sort((a, b) => a - b);
+    nums2 = nums2.sort((a, b) => a - b);
+    var n = nums1.length;
+    var m = nums2.length;
+    var left = 0 , right = 0;
+    var arr = new Array();
+
+    while (left < n && right < m) {
+        if (nums1[left] > nums2[right]) {
+            right++;
+        }else if(nums1[left] < nums2[right]){
+            left++;
+        }else{
+            arr.push(nums1[left]);
+            left++;
+            right++;
+        }
+    }
+   return arr;
+};
+var result0713 = intersect([1,2,2,1]
+    ,[2,2]);
+    console.log(result0713);
+
+
+
+    console.log("2020.7.14");
+    /**
+     * 2020.07.14
+ * @param {number[][]} triangle
+ * @return {number}
+ */
+var minimumTotal = function(triangle) {
+    
+    var n = triangle.length;
+    var dp = new Array(n+1).fill(0);
+    for (let index = n-1; index >= 0; index--) {
+        var m = triangle[index].length
+        for (let j = 0; j < m; j++) {
+            dp[j] = Math.min(dp[j], dp[j+1])+ triangle[index][j]
+        }
+    }
+    console.log(dp);
+    return dp[0];
+}
+var result0714 = minimumTotal([
+    [2],
+   [3,4],
+  [6,5,7],
+ [4,1,8,3]
+]);
+console.log(result0714);
+var result0714P = document.getElementById('result0714');
+
+result0714P.innerHTML = '答案是:'+result0714
